6398affffca1ecad42fc36ee7891747c760e7b90
Anthonyz28 <anthony.z28@hotmail.com>
2020-12-16 22:12:50 -0600

    corrected 3.2

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,6 +8,9 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+if (playerHealth > 0) {
+  window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
+}
 for (var i = 0; i < enemyNames.length; i++) {
   var pickedEnemyName = enemyNames[i];
   enemyHealth = 50;
@@ -59,13 +62,27 @@ var fight = function(enemyName) {
       window.alert("Welcome to Robot Gladiators!");
     } 
      // check player's health
-    if (playerHealth <= 0) {
-      window.alert(playerName + " has died!");
-      break;
-    } else {
-      window.alert(playerName + " still has " + playerHealth + " health left.");
+     if (playerHealth > 0) {
+      // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+      window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+  
+      // pick new enemy to fight based on the index of the enemyNames array
+      var pickedEnemyName = enemyNames[i];
+  
+      // reset enemyHealth before starting new fight
+      enemyHealth = 50;
+  
+      // use debugger to pause script from running and check what's going on at that moment in the code
+      // debugger;
+  
+      // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+      fight(pickedEnemyName);
     }
-    
+
+      else {
+        window.alert("You have lost you robot in battle! Game Over!");
+        break;
+      }
     // if player choses to fight, fight
     if (promptFight === "fight" || promptFight === "FIGHT") {
     
@@ -97,11 +114,11 @@ var fight = function(enemyName) {
       fight();
     }
     // if player did not chose 1 or 2 in prompt
-  } else {
+    else {
     window.alert("You need to pick a valid option. Try again!");
-  }}
- 
-};
+    }
+
+
 
 // run fight function to start game
 for(var i = 0; i < enemyNames.length; i++) {
